9fb62656edf09ea679085f42ab701909851f50b5
RX0kas <116596186+RX0kas@users.noreply.github.com>
2025-12-01 19:38:26 +0100

    Fix Import

--- cpp_backend/turtle_renderer.cpp
+++ cpp_backend/turtle_renderer.cpp
@@ -9,7 +9,6 @@
 #include <GLFW/glfw3.h>
 #include <fstream>
 
-#include "ImFileDialog.h"
 #include "interface.hpp"
 #include "shader.hpp"
 
@@ -379,40 +378,6 @@ void Turtle::clearUndoHistory() {
 
 TurtleRenderer::TurtleRenderer(const char *fPath, const char *vPath) : s(vPath,fPath) {
     updateViewMatrix();
-
-    // Setup ImFileDialog
-    ifd::FileDialog::Instance().CreateTexture = [](uint8_t* data, int w, int h, char fmt) -> void* {
-        GLuint tex = 0;
-
-        glGenTextures(1, &tex);
-        glBindTexture(GL_TEXTURE_2D, tex);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-        glTexImage2D(
-            GL_TEXTURE_2D,
-            0,
-            GL_RGBA,
-            w,
-            h,
-            0,
-            (fmt == 0) ? GL_BGRA : GL_RGBA,
-            GL_UNSIGNED_BYTE,
-            data
-        );
-
-        glGenerateMipmap(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, 0);
-
-        return (void*)(uintptr_t)tex;
-    };
-
-    ifd::FileDialog::Instance().DeleteTexture = [](void* tex) {
-        GLuint texID = (GLuint)(uintptr_t)tex;
-        glDeleteTextures(1, &texID);
-    };
 }
 
 
